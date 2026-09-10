375e262c6bf6b4127acb60ed52b7bc66da0341ec
Kajal <goyalkajal2006@gmail.com>
2026-09-10 09:46:00 +0530

    Add environment variable loading for API key

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 API_KEY = os.getenv("OPENWEATHER_API_KEY")  #OpenWeatherMap API key
 if not API_KEY:
-    raise RuntimeError("OPENWEATHER_API_KEY not set. Did you create a .env file?")
+    raise RuntimeError("OPENWEATHER_API_KEY not set")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
